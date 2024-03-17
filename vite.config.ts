e786aca696d6df1235a4d259286a9ef88c3b7f8e
菲鸽 <1020103647@qq.com>
2024-03-17 15:32:00 +0800

    build: delete useless files

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,50 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 // @see https://unocss.dev/
 import UnoCSS from 'unocss/vite'
 
-export default defineConfig({
-  plugins: [uni(), UnoCSS()],
-  resolve: {
-    alias: {
-      '@': path.join(process.cwd(), './src'),
+// https://vitejs.dev/config/
+export default ({ command, mode }) => {
+  // console.log(mode === process.env.NODE_ENV) // true
+
+  // mode: 区分生产环境还是开发环境
+  console.log(command, mode)
+  // pnpm dev:h5 时得到 => serve development
+  // pnpm build:h5 时得到 => build production
+  // pnpm dev:mp-weixin 时得到 => build development (注意区别，command为build)
+  // pnpm build:mp-weixin 时得到 => build production
+
+  // process.cwd(): 获取当前文件的目录跟地址
+  // loadEnv(): 返回当前环境env文件中额外定义的变量
+  const env = loadEnv(mode, path.resolve(process.cwd(), 'env'))
+  console.log(env)
+  console.log(process.env.UNI_PLATFORM) // 得到 mp-weixin, h5 等
+
+  return defineConfig({
+    // envDir: './env', // 自定义env目录
+    plugins: [uni(), UnoCSS()],
+
+    css: {
+      postcss: {
+        plugins: [
+          // autoprefixer({
+          //   // 指定目标浏览器
+          //   overrideBrowserslist: ['> 1%', 'last 2 versions'],
+          // }),
+        ],
+      },
     },
-  },
-  server: {
-    host: '0.0.0.0',
-    hmr: true,
-    port: 9900,
-  },
-})
+
+    resolve: {
+      alias: {
+        '@': path.join(process.cwd(), './src'),
+      },
+    },
+    server: {
+      host: '0.0.0.0',
+      hmr: true,
+      port: 9900,
+    },
+  })
+}
