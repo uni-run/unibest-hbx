56448956a4875a393f053e9874831221d37d7b7a
菲鸽 <1020103647@qq.com>
2024-03-17 15:28:58 +0800

    feat: vite 版 unocss

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
 import store from './store'
-import './unocss.css'
+import 'virtual:uno.css'
 
 export function createApp() {
   const app = createSSRApp(App)
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
+// @see https://unocss.dev/
+import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [uni(), UnoCSS()],
+  resolve: {
+    alias: {
+      '@': path.join(process.cwd(), './src'),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    hmr: true,
+    port: 9900,
+  },
 })
